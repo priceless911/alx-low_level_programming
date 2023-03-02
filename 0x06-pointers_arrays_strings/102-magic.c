9491e9efb5b9fe95b802f6c29586d0052de23473
priceless911 <officialmonikhet@gmail.com>
2023-03-02 06:17:47 -0800

    test

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,12 +8,12 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/*
-	 * write your line of code here...
+	/**
+	 * write yout line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
-	 * - only one statement
+	 * - only one statment
 	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
