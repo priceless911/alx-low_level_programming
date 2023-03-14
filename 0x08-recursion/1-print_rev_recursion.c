69e0dbab89afc5f097a116ac8e2a17c20d873034
priceless911 <officialmonikhet@gmail.com>
2023-03-14 13:58:27 -0700

    test

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - reverse a string
+ * @str: stirng array
+ * Return: nothing
+*/
+
+void _print_rev_recursion(char *str)
+{
+	if (*str == '\0')
+	{
+		return;
+	}
+
+	_print_rev_recursion(str + 1);
+	_putchar(*str);
+}
