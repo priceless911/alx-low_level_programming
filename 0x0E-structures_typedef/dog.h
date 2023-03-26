625d661c0c6141177fec15a4ed6f4ed8c7b42d39
priceless911 <officialmonikhet@gmail.com>
2023-03-26 12:48:32 -0700

    tast1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,20 @@
-#ifndef _DOG_H_
-#define _DOG_H_
-
+#ifndef DOG_H
+#define DOG_H
 /**
-*** struct dog - a structure to define a dog ************
-*** @name: name of the dog structure ********************
-*** @owner: owner of the dog ****************************
-*** @age: age of the dog ********************************
-**/
-
-typedef struct dog
+* struct dog - struct of a dog
+* @name: name of the dog
+* @age: age if the dog
+* @owner: the owner of the dog
+*/
+struct dog
 {
 	char *name;
-	char *owner;
 	float age;
-} dog_t;
-
+	char *owner;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
