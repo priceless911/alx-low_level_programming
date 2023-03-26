63f7c0d490f3b6170341490969b94b0a956c406d
priceless911 <officialmonikhet@gmail.com>
2023-03-26 12:52:29 -0700

    task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,9 +12,4 @@ struct dog
 	float age;
 	char *owner;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-typedef struct dog dog_t;
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 #endif
